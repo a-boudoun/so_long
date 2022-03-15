6c41796cc1a283f9b71f761686bc512652b92e08
Abderrahim Boudounit <aboudoun@e1r9p9.1337.ma>
2022-03-15 20:12:04 +0100

    done

--- render.c
+++ render.c
@@ -6,7 +6,7 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 20:10:03 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/03/15 19:50:31 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/03/15 20:11:03 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ void	render_player(t_program *program, int j, int i)
 	program->player.y = i;
 	// if (!program->player.position)
 	mlx_put_image_to_window(program->mlx, program->mlx_win,
-		program->img.player_right, i * HEIGHT, j * WIDTH);
-	// else
-	// 	mlx_put_image_to_window(program->mlx, program->mlx_win,
-	// 		program->img.player_right, i * HEIGHT, j * WIDTH);
+		program->img.player_right, j * HEIGHT, i * WIDTH);
+	/* else
+	 	mlx_put_image_to_window(program->mlx, program->mlx_win,
+	 		program->img.player_right, i * HEIGHT, j * WIDTH);*/
 }
 
 void	render_exit(t_program *program, int j, int i)
